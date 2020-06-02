a8e3ea3e5790c18918294c7908c936f51462e3a1
Noah Albright <albright.noahk@gmail.com>
2020-06-02 15:05:47 -0700

    generic stairs and slabs added

--- src/main/java/com/dynashetvala/edenbound/init/BlockInit.java
+++ src/main/java/com/dynashetvala/edenbound/init/BlockInit.java
@@ -3,7 +3,9 @@ package com.dynashetvala.edenbound.init;
 import com.dynashetvala.edenbound.Edenbound;
 import com.dynashetvala.edenbound.Edenbound.BlockTab;
 import net.minecraft.block.Block;
+import net.minecraft.block.SlabBlock;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.StairsBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -20,6 +22,10 @@ public class BlockInit
 {
     public  static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, Edenbound.MOD_ID);
 
-    public static final RegistryObject<Block> LARGE_GENERIC_OBJECT = BLOCKS.register("large_generic_object", () -> new Block(Block.Properties.create(Material.EARTH).hardnessAndResistance(0.5f, 5).sound(SoundType.STONE).harvestTool(ToolType.PICKAXE)));
+    //Perfectly Generic
     public static final RegistryObject<Block> PERFECTLY_GENERIC_ORE = BLOCKS.register("perfectly_generic_ore", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(5.0f, 5).sound(SoundType.STONE).harvestTool(ToolType.PICKAXE)));
+    public static final RegistryObject<Block> LARGE_GENERIC_OBJECT = BLOCKS.register("large_generic_object", () -> new Block(Block.Properties.create(Material.EARTH).hardnessAndResistance(0.5f, 5).sound(SoundType.STONE).harvestTool(ToolType.PICKAXE)));
+    public static final RegistryObject<Block> PERFECTLY_GENERIC_STAIRS = BLOCKS.register("perfectly_generic_stairs", () -> new StairsBlock(() -> LARGE_GENERIC_OBJECT.get().getDefaultState(), Block.Properties.create(Material.EARTH)));
+    public static final RegistryObject<Block> PERFECTLY_GENERIC_SLAB = BLOCKS.register("perfectly_generic_slab", () -> new SlabBlock(Block.Properties.create(Material.EARTH)));
+    //Perfectly Generic
 }
